f7d0216b8d37301476398fbc4c9e3183ed6a01e9
dbsxdbsx <djw0214@gmail.com>
2024-01-21 11:07:39 +0800

    test

--- tests/test.rs
+++ tests/test.rs
@@ -2,14 +2,13 @@
 mod tests {
     use trait_variable::trait_var;
 
-    #[trait_var2]
-    #[test]
-    fn test_procedure_macro_syntax() {
-        println!("test_module");
+    #[trait_var]
+    struct StructName {
+        pub prop: i32,
     }
 
     #[test]
     fn another_test_procedure_macro_syntax() {
-        println!("another_test_module");
+        let s = StructName { prop: 1 };
     }
 }
