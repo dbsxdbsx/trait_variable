042a3bfa624a8fd85b68aa17cd95261317b093cd
dbsxdbsx <djw0214@gmail.com>
2024-01-21 10:48:46 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,55 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-use proc_macro2::Ident;
+// use proc_macro2::Ident;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput, ItemTrait, TraitItem, TraitItemMethod};
 
-#[proc_macro_attribute]
-pub fn trait_var(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attr_str = attr.to_string();
-    let trait_name = parse_macro_input!(attr as Ident);
-    let input = parse_macro_input!(item as DeriveInput);
-    let struct_name = &input.ident;
+use proc_macro::{Ident, Span, TokenStream};
 
-    // 解析特性定义，以便我们可以检查其中的方法
-    let trait_ast: ItemTrait = syn::parse_str(&attr_str).expect("Failed to parse trait");
-
-    // 遍历特性中的所有项目
-    let methods = trait_ast.items.into_iter().filter_map(|item| {
-        if let TraitItem::Method(TraitItemMethod { sig, .. }) = item {
-            // 检查方法名称是否符合特定格式
-            let method_name = &sig.ident;
-            let method_name_str = method_name.to_string();
-            if let Some(stripped) = method_name_str.strip_prefix('_') {
-                // 提取类型名称和字段名称
-                let type_name = &sig.output;
-                let field_name = format_ident!("{}", stripped);
-                // 生成对应的方法实现
-                let generated = quote! {
-                    fn #method_name(&self) -> #type_name {
-                        &self.#field_name
-                    }
-                };
-                return Some(generated);
-            }
-        }
-        None
-    });
-
-    // 生成最终的impl块
-    let gen = quote! {
-        impl #trait_name for #struct_name {
-            #(#methods)*
-        }
-    };
-
-    gen.into()
+#[proc_macro]
+pub fn echo(input: TokenStream) -> TokenStream {
+    input
 }
+
+
+// #[proc_macro_attribute]
+// pub fn trait_var(attr: TokenStream, item: TokenStream) -> TokenStream {
+//     let attr_str = attr.to_string();
+//     let trait_name = parse_macro_input!(attr as Ident);
+//     let input = parse_macro_input!(item as DeriveInput);
+//     let struct_name = &input.ident;
+
+//     // 解析特性定义，以便我们可以检查其中的方法
+//     let trait_ast: ItemTrait = syn::parse_str(&attr_str).expect("Failed to parse trait");
+
+//     // 遍历特性中的所有项目
+//     let methods = trait_ast.items.into_iter().filter_map(|item| {
+//         if let TraitItem::Method(TraitItemMethod { sig, .. }) = item {
+//             // 检查方法名称是否符合特定格式
+//             let method_name = &sig.ident;
+//             let method_name_str = method_name.to_string();
+//             if let Some(stripped) = method_name_str.strip_prefix('_') {
+//                 // 提取类型名称和字段名称
+//                 let type_name = &sig.output;
+//                 let field_name = format_ident!("{}", stripped);
+//                 // 生成对应的方法实现
+//                 let generated = quote! {
+//                     fn #method_name(&self) -> #type_name {
+//                         &self.#field_name
+//                     }
+//                 };
+//                 return Some(generated);
+//             }
+//         }
+//         None
+//     });
+
+//     // 生成最终的impl块
+//     let gen = quote! {
+//         impl #trait_name for #struct_name {
+//             #(#methods)*
+//         }
+//     };
+
+//     gen.into()
+// }
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,19 +1,27 @@
-use trait_variable::trait_var;
+// use trait_variable::trait_var;
+// use trait_variable::my_attr_macro;
 
-/// In this module, the attribute with the argument appears over the enum.
-mod attribute_argument_on_enum {
-    use super::*;
+// /// In this module, the attribute with the argument appears over the enum.
+// mod attribute_argument_on_enum {
+//     use super::*;
 
-    trait MyTrait {
-        fn my_function(&self);
-    }
+//     trait MyTrait {
+//         fn my_function(&self);
+//     }
 
-    #[trait_var(MyTrait)]
-    struct MyStruct;
+//     #[trait_var(MyTrait)]
+//     struct MyStruct;
 
+//     #[test]
+//     fn test_trait_var_macro() {
+//         let my_struct = MyStruct;
+//         my_struct.my_function();
+//     }
+// }
+
+use trait_variable::echo;
+
+mod test_module {
     #[test]
-    fn test_trait_var_macro() {
-        let my_struct = MyStruct;
-        my_struct.my_function();
-    }
+    echo!(println!("test_module"));
 }
