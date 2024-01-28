a47d2ae7a5068dac6c4d9ae13c98f2b527b7809b
dbsxdbsx <djw0214@gmail.com>
2024-01-28 21:37:21 +0800

    refine self issue(not ok yet)

--- src/refine_fn.rs
+++ src/refine_fn.rs
@@ -6,7 +6,7 @@ macro_rules! refine_fn {
         [fns_impls_with_self_mut: $($fns_impls_with_self_mut:tt)*]
         [fns_impls_without_self: $($fns_impls_without_self:tt)*]
         [fns_no_impls: $($fns_no_impls:tt)*]
-        fn $fn_name:ident(& $self:expr, $($arg:tt)*) $(-> $ret_ty:ty)? { $($fn_body:tt)* }
+        fn $fn_name:ident(& $self:tt, $($arg:tt)*) $(-> $ret_ty:ty)? { $($fn_body:tt)* }
         $($rest:tt)*
     ) => (
         $crate::refine_fn! {
@@ -23,7 +23,7 @@ macro_rules! refine_fn {
         [fns_impls_with_self_mut: $($fns_impls_with_self_mut:tt)*]
         [fns_impls_without_self: $($fns_impls_without_self:tt)*]
         [fns_no_impls: $($fns_no_impls:tt)*]
-        fn $fn_name:ident(&mut $self:expr, $($arg:tt)*) $(-> $ret_ty:ty)? { $($fn_body:tt)* }
+        fn $fn_name:ident(&mut $self:tt, $($arg:tt)*) $(-> $ret_ty:ty)? { $($fn_body:tt)* }
         $($rest:tt)*
     ) => (
         $crate::refine_fn! {
@@ -66,8 +66,8 @@ macro_rules! refine_fn {
     });
     // 2.final output
     (
-        [fns_impls_with_self: $( $fn_name_impl_with_self:ident (&$self:expr, $($arg_impl_with_self:tt)*) $($ret_ty_impl_with_self:ty)? {$($fn_body_with_self:tt)*} )*]
-        [fns_impls_with_self_mut: $( $fn_name_impl_with_self_mut:ident (&mut $self_mut:expr, $($arg_impl_with_self_mut:tt)*) $($ret_ty_impl_with_self_mut:ty)? {$($fn_body_with_self_mut:tt)*} )*]
+        [fns_impls_with_self: $( $fn_name_impl_with_self:ident (&$self:tt, $($arg_impl_with_self:tt)*) $($ret_ty_impl_with_self:ty)? {$($fn_body_with_self:tt)*} )*]
+        [fns_impls_with_self_mut: $( $fn_name_impl_with_self_mut:ident (&mut $self_mut:tt, $($arg_impl_with_self_mut:tt)*) $($ret_ty_impl_with_self_mut:ty)? {$($fn_body_with_self_mut:tt)*} )*]
         [fns_impls_without_self: $( $fn_name_impl_without_self:ident ($($arg_impl_without_self:tt)*) $($ret_ty_impl_without_self:ty)? {$($fn_body_without_self:tt)*} )*]
         [fns_no_impls: $( $fn_name_no_impl:ident ($($arg_no_impl:tt)*) $($ret_ty_no_impl:ty)? ; )*]
     ) => (
@@ -115,52 +115,48 @@ macro_rules! refine_fn {
 /// refine the body content of a function/method, excluding braces
 /// replace all `self.<field_name>` to `self._<field_name>()`
 macro_rules! refine_fn_body {
-    // // 0.
-    // (@work_around_self
-    //     $($self:tt)* | $fn_name:ident | $($args:tt)*
-    // ) => {
-    //     $self$fn_name($($args)*)
-    // };
     // 1.1 Match method call with `self.` prefix, match and paste
     (
-        $self:expr,
+        $self:tt,
         [pre_content: $($pre_content:tt)*]
-        self.$fn_name:ident ($($args:tt)*) // match fn call with `self.` prefix
+        $field:ident.$fn_name:ident ($($args:tt)*) // match fn call with `self.` prefix
         $($rest:tt)*
     ) => {
         $crate::refine_fn_body! {
             $self,
             [
                 pre_content: $($pre_content)*
-                $self.$fn_name($($args:tt)*)  // just copy and paste the original fn code
+                $field.$fn_name($($args:tt)*)  // just copy and paste the original fn code
             ]
             $($rest)*
         }
     };
     // 1.2 Match `self.<field_name>` and replace it
     (
-        $self:expr,
+        $self:tt,
         [pre_content: $($pre_content:tt)*]
-        self.$field_name:ident // match field with `self.` prefix
+        $field:ident.$field_name:ident // match field with `self.` prefix
+        // self.$field_name:ident // match field with `self.` prefix
         $($rest:tt)*
     ) => {
         $crate::refine_fn_body! {
             $self,
             [
                 pre_content: $($pre_content)*
-                // todo:
-                paste::paste!{
-                    self.
+                // todo: check if $field matches $self
+                paste::paste! {
+                    $self .
                     [<_ $field_name>]
                     ()
                 }
+                // $crate::match_self_field!($self, $field, $field_name)
             ]
             $($rest)*
         }
     };
     // 2. If no matched pattern, process one token at a time
     (
-        $self:expr,
+        $self:tt,
         [pre_content: $($pre_content:tt)*]
         $token:tt
         $($rest:tt)*
@@ -173,9 +169,36 @@ macro_rules! refine_fn_body {
     };
     // 3. Base case: no more tokens to process
     (
-        $self:expr,
+        $self:tt,
         [pre_content: $($pre_content:tt)*]
     ) => {
         $($pre_content)*
+    };
+}
+
+#[macro_export]
+// macro_rules! match_self_field {
+//     // 当$self和$field相同时，使用这个分支
+//     ($self:tt, self, $field_name:ident) => {
+//         // 在这里执行当$self和$field相等时的代码
+//     };
+//     // 当$self和$field不相同时，使用这个分支
+//     ($self:tt, $field:ident, $field_name:ident) => {
+//         // 在这里执行当$self和$field不相等时的代码
+//     };
+// }
+macro_rules! match_self_field {
+    ($self:tt, $field:ident, $field_name:ident) => {
+        if $self as *const _ == &$field as *const _ {
+            paste::paste! {
+                $self .
+                [<_ $field_name>]
+                ()
+            }
+        } else {
+            paste::paste! {
+                $field.$field_name
+            }
+        }
     };
 }
